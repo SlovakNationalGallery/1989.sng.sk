364015121c0266e92b79e0a4d6d068426b9c13d5
Ernest Walzel <ewalzel@gmail.com>
2019-09-20 11:47:04 +0000

    Create model and migration

---
+++ database/migrations/2019_09_20_112334_create_journal_entries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJournalEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journal_entries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('written_at');
+            $table->text('written_at_raw');
+            $table->text('weather');
+            $table->text('weather_raw');
+            $table->text('content');
+            $table->text('content_raw');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journal_entries');
+    }
+}
