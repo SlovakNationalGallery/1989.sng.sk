5dc34e401ea42e63cd9250d609640bcf28cc787b
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-09-25 11:34:06 +0200

    add unit tests to check slug is automaticaly generated when empty and still can be customized

--- database/migrations/2019_09_25_084238_add_slug_to_topics_table.php
+++ database/migrations/2019_09_25_084238_add_slug_to_topics_table.php
@@ -14,7 +14,7 @@ class AddSlugToTopicsTable extends Migration
     public function up()
     {
         Schema::table('topics', function (Blueprint $table) {
-            $table->string('slug')->unique();
+            $table->string('slug')->default('')->unique();
         });
     }
 
