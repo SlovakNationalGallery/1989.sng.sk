0c2043615a9e9932d95a55a046241f3f92824779
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-09-25 11:20:53 +0200

    update factory

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -4,10 +4,13 @@
 
 use App\Models\Topic;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Topic::class, function (Faker $faker) {
+    $name = $faker->sentence(4);
     return [
-        'name' => $faker->sentence(4),
+        'name' => $name,
+        'slug' => Str::slug($name, '-'),
         'description' => $faker->text(),
     ];
 });
