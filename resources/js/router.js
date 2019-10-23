ac426a38556d19572caf519e1faa90b873d75949
Ernest Walzel <ewalzel@gmail.com>
2019-10-23 15:48:51 +0200

    Api endpoint for searching entries

--- resources/js/router.js
+++ resources/js/router.js
@@ -5,7 +5,7 @@ import JournalEntriesGallery from './components/JournalEntriesGallery'
 const router = new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/journal-entries/:date?', component: JournalEntriesGallery, props: ({params}) => ({ date: params.date }) }
+        { path: '/journal-entries/:date?', component: JournalEntriesGallery, props: ({params, query}) => ({ date: params.date, filter: query.tag }) }
     ]
 })
 
