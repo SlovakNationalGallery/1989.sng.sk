76a903d324a980f1c6976505f4abc7bd2e86d437
Ernest Walzel <ewalzel@gmail.com>
2019-11-06 15:15:23 +0100

    Day switching and navigatio working using vue-carousel

--- resources/js/components/RowView.vue
+++ resources/js/components/RowView.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="cldr-row">
-    <button class='btn btn-dark' @click="prevPeriod()">〈</button>
     <button @click="prevPeriod()">〈</button>
-    <transition-group name="list" class="days-wrapper" :class="direction">
-      <calendar-day
-        v-for="(day) in displayedDays"
-        :onClick="selectDay.bind(this, day)"
-        :key="day.d"
-        :date="day.d"
-        :active="day.active"
-        :selected="selectedDay === day.d"
-      ></calendar-day>
-    </transition-group>
+    <carousel
+      class="carousel"
+      :navigateTo="navigateTo"
+      :scrollPerPage="false"
+      :paginationEnabled="false"
+      :mouse-drag="false"
+      :perPage="perPage"
+    >
+      <slide v-for="(day, i) in days" :key="day.d">
+        <calendar-day
+          class="day"
+          :date="day.d"
+          :active="day.active"
+          :selected="selectedIndex === i"
+          @click="onSlideClick(i)"
+        ></calendar-day>
+      </slide>
+    </carousel>
     <button @click="nextPeriod()">〉</button>
   </div>
 </template>
 
 <script>
 import CalendarDay from './Calendar/Day'
+import { Carousel, Slide } from 'vue-carousel';
 
 export default {
   name: "RowView",
-  components: { CalendarDay },
+  components: { CalendarDay, Carousel, Slide },
   props: {
     startAt: {
       type: String,
@@ -34,76 +42,49 @@ export default {
   },
   data() {
     return {
-      selectedIndex: this.startAt
-        ? this.days.findIndex(a => a.d === this.startAt)
-        : this.days.findIndex(a => a.active),
-      middle: Math.floor(this.displayCount / 2),
-      direction: "left",
-      cnt: this.displayCount * 3,
-      displayWindow: 9,
+      selectedIndex: this.days.findIndex(({d}) => this.startAt === d),
+      perPage: 5,
+      initialNavigateToDone: false,
     };
   },
   created() {
-    this.updateDisplayWindow();
+    this.updatePerPage();
 
-    this.onWindowResize = _.debounce(this.updateDisplayWindow, 200)
+    this.onWindowResize = _.debounce(this.updatePerPage, 200)
     window.addEventListener("resize", this.onWindowResize);
   },
   beforeDestroy() {
     window.removeEventListener("resize", this.onWindowResize);
   },
   watch: {
-    startAt() {
-      this.selectedIndex = this.startAt
-        ? this.days.findIndex(a => a.d === this.startAt)
-        : this.days.findIndex(a => a.active);
-    },
+    selectedIndex() {
+      this.$emit("change", this.days[this.selectedIndex].d);
+    }
   },
   computed: {
-    selectedDay() {
-      return (
-        this.startAt &&
-        this.days[this.selectedIndex] &&
-        this.days[this.selectedIndex].d
-      );
+    navigateTo() {
+      if (!this.initialNavigateToDone) {
+        this.initialNavigateToDone = true
+        return [this.selectedIndex - this.navigationOffset, false]
+      }
+
+      return [this.selectedIndex - this.navigationOffset, true]
     },
-    displayedDays() {
-      const padding = (this.displayWindow - 1) / 2
-      return _
-        .range(-padding, padding + 1)
-        .map(dayIndexOffset => this.days[this.selectedIndex + dayIndexOffset])
+    navigationOffset() {
+      return (this.perPage - 1) / 2
     },
-    startIndex() {
-      return this.days.findIndex(a => a.active);
-    },
-    endIndex() {
-      return this.days.findIndex(a => a.d > this.startAt && !a.active) - 1;
-    }
   },
   methods: {
     prevPeriod() {
-      this.direction = "right";
-      this.selectedIndex = Math.max(
-        this.selectedIndex - this.displayCount,
-        this.startIndex
-      );
-      this.$emit("input", this.days[this.selectedIndex].d);
+      this.selectedIndex = Math.max(this.selectedIndex - this.perPage, 0 + this.navigationOffset)
     },
     nextPeriod() {
-      this.direction = "left";
-      this.selectedIndex = Math.min(
-        this.selectedIndex + this.displayCount,
-        this.endIndex
-      );
-      this.$emit("input", this.days[this.selectedIndex].d);
+      this.selectedIndex = Math.min(this.selectedIndex + this.perPage, this.days.length - this.navigationOffset - 1)
     },
-    selectDay(d) {
-      const target = this.days.indexOf(d);
-      this.direction = target > this.selectedIndex ? "left" : "right";
-      this.selectedIndex = target;
-      this.$emit("input", d.d);
+    onSlideClick(index) {
+      this.selectedIndex = index
     },
-    updateDisplayWindow() {
+    updatePerPage() {
       function minSize(daysCount) {
         const dayWidth = 50
         const gutter = 10
@@ -112,12 +93,12 @@ export default {
         return daysCount * (dayWidth + gutter) + 2 * (buttonWidth + 3 * gutter)
       }
 
-      if (window.innerWidth > minSize(9)) return this.displayWindow = 9
-      if (window.innerWidth > minSize(7)) return this.displayWindow = 7
-      if (window.innerWidth > minSize(5)) return this.displayWindow = 5
-      if (window.innerWidth > minSize(3)) return this.displayWindow = 3
+      if (window.innerWidth > minSize(9)) return this.perPage = 9
+      if (window.innerWidth > minSize(7)) return this.perPage = 7
+      if (window.innerWidth > minSize(5)) return this.perPage = 5
+      if (window.innerWidth > minSize(3)) return this.perPage = 3
 
-      return this.displayWindow = 1
+      return this.perPage = 1
     },
   }
 };
@@ -125,7 +106,7 @@ export default {
 
 <style lang="scss" scoped>
 .cldr-row {
-  $day-width: 50px;
+  $day-width: 60px;
   $gutter: 10px;
   $button-width: 40px;
 
@@ -134,23 +115,24 @@ export default {
 
   button {
     width: $button-width;
+    height: $day-width;
+    color: white;
   }
 
-  .days-wrapper {
+  .carousel {
     display: flex;
-    overflow-x: hidden;
     margin-left: 2 * $gutter;
     margin-right: 2 * $gutter;
-    max-width: 9 * ($day-width + $gutter) - $gutter;
+    width: 9 * ($day-width + $gutter) - $gutter;
 
-    @media screen and (max-width: 9 * ($day-width + $gutter) + 2 * ($button-width + 3 * $gutter)) {
-      max-width: 7 * ($day-width + $gutter) - $gutter;
+    @media screen and (width: 9 * ($day-width + $gutter) + 2 * ($button-width + 3 * $gutter)) {
+      width: 7 * ($day-width + $gutter) - $gutter;
     }
-    @media screen and (max-width: 7 * ($day-width + $gutter) + 2 * ($button-width + 3 * $gutter)) {
-      max-width: 5 * ($day-width + $gutter) - $gutter;
+    @media screen and (width: 7 * ($day-width + $gutter) + 2 * ($button-width + 3 * $gutter)) {
+      width: 5 * ($day-width + $gutter) - $gutter;
     }
-    @media screen and (max-width: 5 * ($day-width + $gutter) + 2 * ($button-width + 3 * $gutter)) {
-      max-width: 3 * ($day-width + $gutter) - $gutter;
+    @media screen and (width: 5 * ($day-width + $gutter) + 2 * ($button-width + 3 * $gutter)) {
+      width: 3 * ($day-width + $gutter) - $gutter;
     }
     @media screen and (max-width: 2 * ($day-width + $gutter) + 2 * ($button-width + 3 * $gutter)) {
       width: 1 * ($day-width + $gutter) - $gutter;
