e314539eb4769539040a913c49b5c269e349a402
Ernest Walzel <ewalzel@gmail.com>
2019-11-08 20:31:24 +0100

    Text item component (first version)

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,5 +7,6 @@ Vue.component('calendar', require('./components/Calendar.vue').default);
 Vue.component('journal-entry', require('./components/JournalEntry.vue').default);
 Vue.component('day-entry', require('./components/DayEntry.vue').default);
 Vue.component('transition-page', require('./components/TransitionPage.vue').default);
+Vue.component('item-text', require('./components/ItemText.vue').default);
 
 new Vue({ router: window.Router, el: '#app' })
