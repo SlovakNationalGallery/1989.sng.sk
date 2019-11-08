44504ba1524f32e98b43b9a5a1e8b590f2386deb
Ernest Walzel <ewalzel@gmail.com>
2019-11-08 19:14:03 +0100

    Image item component

--- resources/views/components/items/image.blade.php
+++ resources/views/components/items/image.blade.php
@@ -1,1 +1,8 @@
-<img src="{{ asset($item->file) }}" alt="{{ $item->full_name }}" />
+<div class="item-image">
+    <div class="item-image-background">
+        <div class="item-image-container">
+            <img src="{{ asset($item->file) }}" alt="{{ $item->full_name }}" />
+        </div>
+        <div class="item-image-description">{{ $item->full_name }}</div>
+    </div>
+</div>
