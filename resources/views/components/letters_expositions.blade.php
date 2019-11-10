24a73f2876d4f099499863347f94df86f143c15e
Martin Havala <martin.havala@gmail.com>
2019-11-10 10:09:54 +0100

    no message

--- resources/views/components/letters_expositions.blade.php
+++ resources/views/components/letters_expositions.blade.php
@@ -3,18 +3,14 @@
 
       <div id="expos" class="shift-block bg-v4">
          <h4>Výstavy a podujatia</h4>
-         <div class="author">Slovenská národná galéria</div>
          <div>(doplníme)</div>
-         {{-- <button class="btn btn-dark"><a href="http://sng.sk">Prejsť na stránku</a></button> --}}
       </div>
    </div>
    <div class="shift-block-container">
 
       <div id="letters" class="shift-block bg-v1">
          <h4>Edukačné aktivity</h4>
-         {{-- <div class="author"> Juraj Smatana</div> --}}
          <div>(doplníme)</div>
-         {{-- <button class="btn"><a href="#TODO">Prejsť na stránku</a></button> --}}
       </div>
    </div>
 
