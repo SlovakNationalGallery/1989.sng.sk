fa2ef7e248b08e8c1fec84611d15e60d481f4f46
Martin Havala <martin.havala@gmail.com>
2019-11-10 10:01:17 +0100

    new topics soon

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,18 +1,28 @@
 <div id="footer" class="container">
-
-    @foreach (App\Models\Topic::$available_categories as $catKey => $cat)
-    @if (isset($topics[$catKey]))
-    <dl>
-        <dt>{{ $cat }}</dt>
-        @foreach ($topics[$catKey] as $topic)
-        @if ($topic->is_active)
-            <dd class="active"><a href="/{{ $topic->slug }}">{{ $topic->name }}</a></dd>
-        @else
-            <dd class="inactive">{{ $topic->name }}</dd>
-        @endif
-        @endforeach
-    </dl>
-    @endif
-    @endforeach
+    <div class="row">
+        <div class="col-sm-12 topics">
+            @foreach (App\Models\Topic::$available_categories as $catKey => $cat)
+            @if (isset($topics[$catKey]))
+            <dl>
+                <dt>{{ $cat }}</dt>
+                @foreach ($topics[$catKey] as $topic)
+                @if ($topic->is_active)
+                <dd class="active"><a href="/{{ $topic->slug }}">{{ $topic->name }}</a></dd>
+                @else
+                <dd class="inactive">{{ $topic->name }}</dd>
+                @endif
+                @endforeach
+            </dl>
+            @endif
+            @endforeach
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12 new-topics">
+            <h3>
+                Nové témy <br/>pribudnú čoskoro
+            </h3>
+        </div>
+    </div>
 
 </div>
