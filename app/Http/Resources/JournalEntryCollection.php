f80234ec229bb2a6937e4e432a930bcf8bf9eef5
Ernest Walzel <ewalzel@gmail.com>
2019-11-11 14:45:54 +0100

    Revert unintended change

--- app/Http/Resources/JournalEntryCollection.php
+++ app/Http/Resources/JournalEntryCollection.php
@@ -18,7 +18,6 @@ class JournalEntryCollection extends ResourceCollection
             'data' => $this->collection->map(function($journalEntry) {
                 return [
                     'written_at' => $journalEntry->written_at->toDateString(),
-                    'is_active' => $journalEntry->is_active
                 ];
             })
         ];
