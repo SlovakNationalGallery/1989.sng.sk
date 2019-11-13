5fb241a0c1b7548bf36d7722b933ffb4d065bdd3
Martin Havala <martin.havala@gmail.com>
2019-11-13 13:32:01 +0100

    feature/WEBUMENIA-1151: quotation

--- resources/js/components/ItemText.vue
+++ resources/js/components/ItemText.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="item-text">
     <div v-html="displayedContent" class="item-text-content"></div>
-
+    
+    <div class="item-fade" v-if="truncateable && !showAll" ></div>
+    <div class="item-credits" v-if="credits" v-html="credits"></div>
     <div v-if="truncateable" class="item-text-read-more-link">
-      <button type="button" class="btn btn-link" @click="showAll = !showAll">{{ showAll ? 'Zatvoriť ↑' : 'Celý text ↓'}}</button>
+      <button type="button" class="btn btn-link" @click="showAll = !showAll">
+        {{ showAll ? "Zatvoriť ↑" : "Celý text ↓" }}
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-const MAX_LENGTH = 500
-const READ_MORE_DELIMITER = '<hr />'
+const MAX_LENGTH = 500;
+const READ_MORE_DELIMITER = "<hr />";
 
 export default {
   name: "ItemText",
   props: {
     content: {
       type: String,
-      required: true,
+      required: true
     },
+    credits: {
+      type: String,
+      required: false
+    }
   },
   data() {
     return {
@@ -27,15 +35,15 @@ export default {
   },
   computed: {
     displayedContent() {
-      if (this.showAll) return this.content.replace(READ_MORE_DELIMITER, '')
+      if (this.showAll) return this.content.replace(READ_MORE_DELIMITER, "");
 
-      const delimiterIndex = this.content.indexOf(READ_MORE_DELIMITER)
-      if (delimiterIndex > -1) return this.content.substring(0, delimiterIndex)
+      const delimiterIndex = this.content.indexOf(READ_MORE_DELIMITER);
+      if (delimiterIndex > -1) return this.content.substring(0, delimiterIndex);
 
-      return _.truncate(this.content, { length: MAX_LENGTH, separator: ' '})
+      return _.truncate(this.content, { length: MAX_LENGTH, separator: " " });
     },
     truncateable() {
-      return this.content.length > MAX_LENGTH
+      return this.content.length > MAX_LENGTH;
     }
   }
 };
