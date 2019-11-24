7d84b8b152e66fa3f8beaa37e33cb4e775a1d623
Ernest Walzel <ewalzel@gmail.com>
2019-11-24 21:49:08 +0100

    Fix mobile top padding

--- resources/views/journal_entries.blade.php
+++ resources/views/journal_entries.blade.php
@@ -3,9 +3,9 @@
 @section('page_description', Str::limit(strip_tags($journalEntry->content), 300))
 
 @section('content')
-<div id="journal-entries" class="container mt-5">
+<div id="journal-entries" class="container">
     <div class="row">
-        <div class="offset-lg-2 col-lg-8 bg-white pt-5 px-md-5">
+        <div class="offset-lg-2 col-lg-8 bg-white pt-3 pt-md-5 px-md-5">
             <a href="/" class="btn btn-outline-dark btn-sm">← Domov</a>
             <div id="header" class="text-center mt-4 mt-md-0">
                 <h2 class="py-0">Čas-opis</h2>
