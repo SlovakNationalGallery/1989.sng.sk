74afd3854162993b25230c403282129508e03292
Igor Rjabinin <igor.rjabinin@gmail.com>
2019-12-05 14:44:32 +0100

    hide menu button on on scroll down

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -60,7 +60,40 @@ $topics = App\Models\Topic::listing()
 <script>
     $('#menu-button').click(()=> {
         $('body').toggleClass('nav-active');
-    })
+    });
+
+    // Hide menu button on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 10;
+    var navbarHeight = $('#menu-button').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+        if (st > lastScrollTop && st > navbarHeight){
+            $('#menu-button').addClass('nav-up');
+        } else {
+            if(st + $(window).height() < $(document).height()) {
+                $('#menu-button').removeClass('nav-up');
+            }
+        }
+
+        lastScrollTop = st;
+    }
 </script>
 
 @endpush
