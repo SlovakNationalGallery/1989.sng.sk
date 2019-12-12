77be06dbe03bf90b171efeabf0fcd4b30a9615c1
Ernest Walzel <ewalzel@gmail.com>
2019-12-12 10:38:36 +0100

    Extract top-nav-logo

--- resources/views/journal_entries.blade.php
+++ resources/views/journal_entries.blade.php
@@ -4,11 +4,15 @@
 
 @section('content')
 <div id="journal-entries" class="container">
+    <div class="row text-center">
+        <div class="col text-center pt-2 pt-md-3 pb-5">
+            <a href="/" id="logo-top-nav" class="text-decoration-none" title="Prejsť na úvodnú stránku">
+                <h3>ČAS-OPIS <span class="year">1989</span></h3>
+            </a>
+        </div>
+    </div>
     <div class="row">
-        <div class="offset-lg-2 col-lg-8 bg-white pt-3 pt-md-4 px-md-5">
-            <div class="text-right text-md-left">
-                <a href="/" class="btn btn-outline-dark btn-sm">← Domov</a>
-            </div>
+        <div class="offset-lg-2 col-lg-8 bg-white pt-3 pt-md-5 px-md-5">
             <div id="header" class="text-center mt-4 mt-md-0">
                 <h2 class="py-0">Čas-opis</h2>
                 <h5 id="subtitle" class="text-muted">Denník Júliusa Kollera</h5>
